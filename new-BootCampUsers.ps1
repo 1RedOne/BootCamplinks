42c61d24a7479728a99287808466172bbc715d45
1RedOne <sred13@gmail.com>
2015-07-08 13:06:27 -0400

    Added User creation script

---
+++ new-BootCampUsers.ps1
@@ -0,0 +1,19 @@
+﻿#BootCamp new users
+$names = import-csv C:\powershell\usernames.csv
+
+if ($OUs -eq $null){
+    "making OUs"
+    $OUs = "Accounting","IT","Engineering","Marketing" | % {New-ADOrganizationalUnit $_ -PassThru }
+    }
+
+ForEach ($OU in $Ous) {
+
+     1..15 | ForEach { 
+        $first = Get-Random $names.First_Name
+        $last  = Get-Random $names.Last_Name
+
+    "$first $last"
+    new-aduser -name "$First $last" -Path $OU -WhatIf
+    
+    }
+}
