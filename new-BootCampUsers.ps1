a42fedde98d69ac8818a4ea7b1795d5df33a8fc5
1RedOne <sred13@gmail.com>
2015-07-08 17:19:44 -0400

    Published the survival guide preview

---
+++ new-BootCampUsers.ps1
@@ -0,0 +1,19 @@
+﻿#BootCamp new users
+$names = import-csv C:\powershell\usernames.csv
+
+if ($OUs -eq $null){
+    "making OUs"
+    $OUs = "Accounting","IT","Engineering","Marketing" | % {New-ADOrganizationalUnit $_ -PassThru }
+    }
+
+ForEach ($OU in $Ous) {
+
+     1..15 | ForEach { 
+        $first = Get-Random $names.First_Name
+        $last  = Get-Random $names.Last_Name
+
+    "$first $last Logon Name : $($first[0]).$last"
+    new-aduser -name "$First $last" -GivenName $first -Surname $last -SamAccountName "$($first[0]).$last" -Path $OU 
+    
+    }
+}
